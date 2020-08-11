ce42ee91fa62a9b96846f9b7d3922ebceb4c4280
WalterHoban <43675025+WalterHoban@users.noreply.github.com>
2020-08-11 14:25:11 -0400

    Merge the Development into Master for Nuget build  (#4)
    * Add new blazor class library project and demo project. (#1)
    
    * Usert/yong/34754 create all input components (#2)
    
    * Add new blazor class library project and demo project.
    
    * Create all the core input components AB#34754
    
    * Add multiple selection component and time component (#3)
    
    Co-authored-by: yongguan2001 <67647301+yongguan2001@users.noreply.github.com>

---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core.Demo/Constants/StatusEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSD.MSS.Blazor.Components.Core.Demo.Constants
+{
+    public enum StatusEnum : int
+    {
+        New = 1,
+        InProgress = 2,
+        Pending = 3,
+        Complete = 4
+    }
+}
---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core/Models/CheckboxItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSD.MSS.Blazor.Components.Core.Models
+{
+    public class SelectListItem
+    {
+        public string Text { get; set; }
+        public string Id { get; set; }
+        public bool Value { get; set; }
+    }
+}
