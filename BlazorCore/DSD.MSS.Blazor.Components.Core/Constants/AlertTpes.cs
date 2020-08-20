5ab645040745ff8e0ea0a0b02a207c9abac3072b
Kajal Chaudhari <68065247+kajal-tc@users.noreply.github.com>
2020-08-20 09:56:18 -0400

    Users/kajal/43435 add alert component (#5)
    * Added AlertComponent and added bootstrap.css  and updated the component.css
    
    * Added the .sass files for the new bootstrap theme
    
    * Implemented design system for the input components #39177

---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core/Constants/AlertTpes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSD.MSS.Blazor.Components.Core.Constants
+{
+    public enum AlertTypes : int
+    {
+        Success = 1,
+        Warning = 2,
+        Information = 3,
+        Danger = 4,
+        Primary = 5
+    }
+
+}
