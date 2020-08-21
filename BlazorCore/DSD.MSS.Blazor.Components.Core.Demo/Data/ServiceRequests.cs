37ff7b088982cfaf2cc95147f0c8135d264a196f
kajal.chaudhari <kajal.chaudhari@tc.gc.ca>
2020-08-21 15:54:18 -0400

    Added RequestBoard with the static data to the Demo Project to update the theme

---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core.Demo/Data/ServiceRequests.cs
@@ -0,0 +1,47 @@
+﻿using DSD.MSS.Blazor.Components.Core.Demo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSD.MSS.Blazor.Components.Core.Demo.Data
+{
+    public class ServiceRequests
+    {
+        public static List<ServiceRequestModel> GetServiceRequests()
+        {
+            List<ServiceRequestModel> serviceRequests = new List<ServiceRequestModel>();
+            serviceRequests.Add(new ServiceRequestModel()
+            {
+                RequestId = "0000000001",
+                RegistryType = "Small Vessel",
+                RequestType = "First Registry",
+                AssigneeEmployeeName = "Kajal Chaudhari",
+                RequestStatus = "In Progress",
+                RequestorName = "Bob Ferguson",
+                TimeBeforeDeadline = "30 Days"
+            });
+            serviceRequests.Add(new ServiceRequestModel()
+            {
+                RequestId = "0000000002",
+                RegistryType = "Large Vessel",
+                RequestType = "Re-registry",
+                AssigneeEmployeeName = "Kajal Chaudhari",
+                RequestStatus = "In Progress",
+                RequestorName = "Peter Williams",
+                TimeBeforeDeadline = "2 days overdue"
+            });
+            serviceRequests.Add(new ServiceRequestModel()
+            {
+                RequestId = "0000000003",
+                RegistryType = "Large Vessel",
+                RequestType = "First Registry",
+                AssigneeEmployeeName = "Kajal Chaudhari",
+                RequestStatus = "Close",
+                RequestorName = "Nancy Smith",
+                TimeBeforeDeadline = "2 days"
+            });
+            return serviceRequests;
+        }
+    }
+}
