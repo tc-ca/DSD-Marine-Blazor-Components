2f3dcacff17d95171aeb0be7d4455f80b1c52ddf
yongguan2001 <67647301+yongguan2001@users.noreply.github.com>
2020-08-24 14:05:59 -0400

    Users/ramtin/43574 update css for typography side bar top bar ab#43574 (#6)
    * Added RequestBoard with the static data to the Demo Project to update the theme
    
    * Update CSS for Typograpghy, side bar and top bar.
    
    Co-authored-by: kajal.chaudhari <kajal.chaudhari@tc.gc.ca>

---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core.Demo/Pages/BasePage.razor.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Threading.Tasks;
+using DSD.MSS.Blazor.Components.Core.Demo.Utilities;
+
+namespace DSD.MSS.Blazor.Components.Core.Demo.Pages
+{
+    public class BasePageComponent : ComponentBase
+    {
+        [Inject] NavigationManager navigationManager { get; set; }
+        [Inject] IJSRuntime jsRuntime { get; set; }
+        [Inject] SharedResourceMgt localizer { get; set; }
+
+        public string currentRelativePath;
+        public string PageTitle;
+        [Parameter]
+        public string LanguageCode { get; set; } = "en";
+
+        void LocationChanged(object sender, LocationChangedEventArgs e)
+        {
+            var newRelativePath = navigationManager.ToBaseRelativePath(navigationManager.Uri).ToLower();
+
+            if ((currentRelativePath.StartsWith("en", StringComparison.OrdinalIgnoreCase) && newRelativePath.StartsWith("fr", StringComparison.OrdinalIgnoreCase)) ||
+               (currentRelativePath.StartsWith("fr", StringComparison.OrdinalIgnoreCase) && newRelativePath.StartsWith("en", StringComparison.OrdinalIgnoreCase)))
+            {
+                navigationManager.NavigateTo(navigationManager.Uri, true);
+                return;
+            }
+        }
+
+        public void SetSwitchLanguage()
+        {
+            navigationManager.LocationChanged += LocationChanged;
+            string path = navigationManager.ToBaseRelativePath(navigationManager.Uri);
+            currentRelativePath = path.ToLower();
+            String[] pathArray = currentRelativePath.Split('/');
+            LanguageCode = string.IsNullOrEmpty(pathArray[0]) ? "en" : pathArray[0];
+            string switchLanguage = LanguageCode.Equals("en", StringComparison.OrdinalIgnoreCase) ? "fr" : "en";
+            string switchCulture = LanguageCode.Equals("en", StringComparison.OrdinalIgnoreCase) ? "fr-CA" : "en-CA";
+            path = path.Length >= 2 ? path.Substring(2).Replace("#", "") : "";
+
+            navigationManager.NavigateTo($"{switchLanguage}{path}", true);         
+        }
+
+        protected override void OnInitialized()
+        {
+            string path = navigationManager.ToBaseRelativePath(navigationManager.Uri);
+            currentRelativePath = path.ToLower();
+            String[] pathArray = currentRelativePath.Split('/');
+            LanguageCode = string.IsNullOrEmpty(pathArray[0]) ? "en" : pathArray[0];
+            base.OnInitialized();
+        }
+    }
+}
