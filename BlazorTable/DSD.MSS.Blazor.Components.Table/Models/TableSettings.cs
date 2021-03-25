4d37efcb7a7f7a49c6e629a7e7093c3d7a7f3bf5
yongguan2001 <yong.guan@tc.gc.ca>
2021-03-25 15:38:21 -0400

    Update table to support show/hide column, return setting data once filter is updated.

---
+++ BlazorTable/DSD.MSS.Blazor.Components.Table/Models/TableSettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSD.MSS.Blazor.Components.Table.Models
+{
+    /// <summary>
+    /// Table settings
+    /// </summary>
+    /// <typeparam name="TableItem"></typeparam>
+    public class TableSettings<TableItem>
+    {
+        /// <summary>
+        /// Page Size
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// Page Number
+        /// </summary>
+        public int PageNumber { get; set; }
+
+        /// <summary>
+        /// Global Search
+        /// </summary>
+        public string GlobalSearch { get; set; }
+
+        /// <summary>
+        /// Columns
+        /// </summary>
+        public List<IColumn<TableItem>> Columns { get; } = new List<IColumn<TableItem>>();
+    }
+}
