ad250d42983edd8858077321c7b875bb5ac78c6d
ramtinhosseiniTC <67646952+ramtinhosseiniTC@users.noreply.github.com>
2021-04-08 15:24:57 -0400

    Created initial Sharable Collapse Component (#33)
    * Created initial Sharable Collapse Component
    
    * Added Collapse Functionality
    
    Co-authored-by: ramtin.hosseini <ramtin.hosseini@034gc.onmicrosoft.com>

---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core/Components/Collapse.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSD.MSS.Blazor.Components.Core.Components
+{
+    public partial class Collapse
+    {
+        [Parameter] public string Icon { get; set; }
+        [Parameter] public string Title { get; set; }
+        [Parameter] public string HeaderBackgroundColor { get; set; }
+        [Parameter] public string HeaderTitleTextColor { get; set; }
+        [Parameter] public bool ShowBorder { get; set; }
+        [Parameter] public RenderFragment BodyContent { get; set; }
+        [Parameter] public bool IsCollapsable { get; set; } = false;
+        [Parameter] public bool IsCollapsed { get; set; } = false;
+
+        private void SetCollapseState()
+        {
+            IsCollapsed = !IsCollapsed;
+        }
+    }
+}
