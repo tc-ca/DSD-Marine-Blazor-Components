82224c4a432dae5b583e664169325e821a36fb30
ramtin.hosseini <ramtin.hosseini@034gc.onmicrosoft.com>
2021-04-08 14:16:23 -0400

    Created initial Sharable Collapse Component

---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core/Components/Collapse.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSD.MSS.Blazor.Components.Core.Components
+{
+    public partial class Collapse
+    {
+        [Parameter] public string Icon { get; set; }
+        [Parameter] public string Title { get; set; }
+        [Parameter] public string HeaderBackgroundColor { get; set; }
+        [Parameter] public string HeaderTitleTextColor { get; set; }
+        [Parameter] public bool ShowBorder { get; set; }
+        [Parameter] public RenderFragment BodyContent { get; set; }
+    }
+}
