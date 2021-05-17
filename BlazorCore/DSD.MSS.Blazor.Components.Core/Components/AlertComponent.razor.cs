08551dafe0a6d36ee99b86c05c0e14899d19d783
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2021-05-17 14:04:25 -0400

    Develop (#39)
    * Fixed page number issue for table reset function. (#32)
    
    * Created initial Sharable Collapse Component (#33)
    
    * Created initial Sharable Collapse Component
    
    * Added Collapse Functionality
    
    Co-authored-by: ramtin.hosseini <ramtin.hosseini@034gc.onmicrosoft.com>
    
    * Users/yong/81548 fix page number issue for reset table ab#81548 (#35)
    
    * Fixed page number issue for table reset function.
    
    * Fixd the issue when OnAfterDataLoaded not set. Also reset the page number on table reset.
    
    * Modified  the existing alert component to be more modular. Simplified the html code in the .razor file. Created a code behind file for the AlertComponent.  Removed the line in the component.css that was unnecessarily adding a padding to  the .alert-icon .alert-dismissible .close css  class. (#38)
    
    * Added font awesome to the DSD.MSS.Blazor.Components.Core project. (#37)
    
    Co-authored-by: Yong Guan <67647301+yongguan2001@users.noreply.github.com>
    Co-authored-by: ramtinhosseiniTC <67646952+ramtinhosseiniTC@users.noreply.github.com>
    Co-authored-by: ramtin.hosseini <ramtin.hosseini@034gc.onmicrosoft.com>

---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core/Components/AlertComponent.razor.cs
@@ -0,0 +1,78 @@
+﻿namespace DSD.MSS.Blazor.Components.Core.Components
+{
+    using DSD.MSS.Blazor.Components.Core.Constants;
+    using Microsoft.AspNetCore.Components;
+
+    public partial class AlertComponent
+    {
+        [Parameter] 
+        public string Title { get; set; }
+
+        [Parameter] 
+        public string Message { get; set; }
+
+        [Parameter]
+        public bool ShowIcon { get; set; } = true;
+
+        [Parameter] 
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter] 
+        public bool EnableShow { get; set; }
+
+        [Parameter]
+        public bool EnableCancel { get; set; }
+
+        [Parameter] 
+        public AlertTypes AlertType { get; set; }
+
+        [Parameter] 
+        public bool Dismisable { get; set; } = false;
+
+        protected string AlertTypeCssString
+        {
+            get
+            {
+                switch(this.AlertType)
+                {
+                    case AlertTypes.Danger:
+                        return "alert-danger";
+                    case AlertTypes.Information:
+                        return "alert-info";
+                    case AlertTypes.Success:
+                        return "alert-success";
+                    case AlertTypes.Warning:
+                        return "alert-warning";
+                    case AlertTypes.Primary:
+                    default:
+                        return "alert-primary";
+                }
+            }
+        }
+
+        protected  string AlertFontAwesomeIconString
+        {
+            get
+            {
+                switch (this.AlertType)
+                {
+                    case AlertTypes.Danger:
+                        return "fa-ban";
+                    case AlertTypes.Success:
+                        return "fa-check-circle";
+                    case AlertTypes.Warning:
+                        return "fa-exclamation-triangle";
+                    case AlertTypes.Information:
+                        return "fa-exclamation-circle";
+                    case AlertTypes.Primary:
+                    default:
+                        return "fa-flag";
+                }
+            }
+        }
+        protected void HandleCancelClick()
+        {
+            EnableShow = false;
+        }
+    }
+}
