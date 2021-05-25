09a73461eb3d8c12276bc2172afd07d106ebf6c8
TC-Cory <cory.chenier@tc.gc.ca>
2021-05-25 18:32:05 -0400

    Added additional parameter to InputMultipleSelect
    Separated the code from the razor file
    Added UseSelectionAsSelectTitle parameter to allow changing the select title to reflect selected input

---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core/Components/InputMultipleSelect.razor.cs
@@ -0,0 +1,52 @@
+﻿using DSD.MSS.Blazor.Components.Core.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSD.MSS.Blazor.Components.Core.Components
+{
+    public partial class InputMultipleSelect
+    {
+        /// <summary>
+        /// Title for multiple selection
+        /// </summary>
+        [Parameter]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Title for multiple selection
+        /// </summary>
+        [Parameter]
+        public string SelectTitle { get; set; }
+
+        /// <summary>
+        /// Flag that indicates to use selected items as the SelectTitle
+        /// </summary>
+        [Parameter]
+        public bool UseSelectionAsSelectTitle { get; set; } = false;
+        
+        /// <summary>
+        /// Specifies the Field ID
+        /// </summary>
+        [Parameter]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Checkbox list
+        /// </summary>
+        [Parameter]
+        public List<SelectListItem> CheckBoxList { get; set; }
+        
+        /// <summary>
+        /// Gets all selected items 
+        /// </summary>
+        public List<SelectListItem> SelectedItems => this.CheckBoxList.Where(x => x.Value).ToList();
+
+        /// <summary>
+        /// Gets all text from selected items, comma seperated
+        /// </summary>
+        public string SelectedText => string.Join(',', this.SelectedItems.Select(x => x.Text));
+    }
+}
