b7ade1a1d5c8e057206d5ae1ce10c93bec740098
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2021-05-28 17:08:37 -0400

    Develop (#42)
    * Fixed page number issue for table reset function. (#32)
    
    * Created initial Sharable Collapse Component (#33)
    
    * Created initial Sharable Collapse Component
    
    * Added Collapse Functionality
    
    Co-authored-by: ramtin.hosseini <ramtin.hosseini@034gc.onmicrosoft.com>
    
    * Users/yong/81548 fix page number issue for reset table ab#81548 (#35)
    
    * Fixed page number issue for table reset function.
    
    * Fixd the issue when OnAfterDataLoaded not set. Also reset the page number on table reset.
    
    * Modified  the existing alert component to be more modular. Simplified the html code in the .razor file. Created a code behind file for the AlertComponent.  Removed the line in the component.css that was unnecessarily adding a padding to  the .alert-icon .alert-dismissible .close css  class. (#38)
    
    * Added font awesome to the DSD.MSS.Blazor.Components.Core project. (#37)
    
    * Added additional parameter to InputMultipleSelect (#40)
    
    * Added additional parameter to InputMultipleSelect
    
    Separated the code from the razor file
    Added UseSelectionAsSelectTitle parameter to allow changing the select title to reflect selected input
    
    * Made a change to how the SelectTitle will show when there are no selected items and the UseSelectionAsSelectTitle is true.
    
    * Changed the selected items to be seperated by spans with the  badge theme from bootstrap. Added custom CSS to make the badges a little bigger than in bootstrap so that they fit nicely when selected.
    
    * Added a RenderingFragment to allow customizing of how to show SeletectedItems will display to the user.
    
    * Added code comments to the SelectedItemTemplate paremeter.
    
    Co-authored-by: AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
    
    Co-authored-by: Yong Guan <67647301+yongguan2001@users.noreply.github.com>
    Co-authored-by: ramtinhosseiniTC <67646952+ramtinhosseiniTC@users.noreply.github.com>
    Co-authored-by: ramtin.hosseini <ramtin.hosseini@034gc.onmicrosoft.com>
    Co-authored-by: TC-Cory <80551174+TC-Cory@users.noreply.github.com>

---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core/Components/InputMultipleSelect.razor.cs
@@ -0,0 +1,58 @@
+﻿using DSD.MSS.Blazor.Components.Core.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DSD.MSS.Blazor.Components.Core.Components
+{
+    public partial class InputMultipleSelect
+    {
+        /// <summary>
+        /// Title for multiple selection
+        /// </summary>
+        [Parameter]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Title for multiple selection
+        /// </summary>
+        [Parameter]
+        public string SelectTitle { get; set; }
+
+        /// <summary>
+        /// Flag that indicates to use selected items as the SelectTitle
+        /// </summary> 
+        [Parameter]
+        public bool UseSelectedItemsAsSelectTitle { get; set; } = false;
+        
+        /// <summary>
+        /// Specifies the Field ID
+        /// </summary>
+        [Parameter]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Checkbox list
+        /// </summary>
+        [Parameter]
+        public List<SelectListItem> CheckBoxList { get; set; }
+
+        /// <summary>
+        /// Render fragment for customizing how the selected items will display.
+        /// </summary>
+        [Parameter]
+        public RenderFragment<string> SelectedItemTemplate { get; set; }
+        
+        /// <summary>
+        /// Gets all selected items 
+        /// </summary>
+        private List<SelectListItem> SelectedItems => this.CheckBoxList.Where(x => x.Value).ToList();
+
+        /// <summary>
+        /// Flag to determine whether to show the selected items as the select title for the input multiple select
+        /// </summary>
+        private bool ShowSelectedItemsAsSelectTitle => this.UseSelectedItemsAsSelectTitle && this.SelectedItems.Count > 0;
+    }
+}
