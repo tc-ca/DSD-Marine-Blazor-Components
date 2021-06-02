90e93bc141a80192b72efb9e00315d29f98b5e6c
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2021-06-02 10:45:36 -0400

    Develop (#44)
    * Fixed page number issue for table reset function. (#32)
    
    * Created initial Sharable Collapse Component (#33)
    
    * Created initial Sharable Collapse Component
    
    * Added Collapse Functionality
    
    Co-authored-by: ramtin.hosseini <ramtin.hosseini@034gc.onmicrosoft.com>
    
    * Users/yong/81548 fix page number issue for reset table ab#81548 (#35)
    
    * Fixed page number issue for table reset function.
    
    * Fixd the issue when OnAfterDataLoaded not set. Also reset the page number on table reset.
    
    * Modified  the existing alert component to be more modular. Simplified the html code in the .razor file. Created a code behind file for the AlertComponent.  Removed the line in the component.css that was unnecessarily adding a padding to  the .alert-icon .alert-dismissible .close css  class. (#38)
    
    * Added font awesome to the DSD.MSS.Blazor.Components.Core project. (#37)
    
    * Added additional parameter to InputMultipleSelect (#40)
    
    * Added additional parameter to InputMultipleSelect
    
    Separated the code from the razor file
    Added UseSelectionAsSelectTitle parameter to allow changing the select title to reflect selected input
    
    * Made a change to how the SelectTitle will show when there are no selected items and the UseSelectionAsSelectTitle is true.
    
    * Changed the selected items to be seperated by spans with the  badge theme from bootstrap. Added custom CSS to make the badges a little bigger than in bootstrap so that they fit nicely when selected.
    
    * Added a RenderingFragment to allow customizing of how to show SeletectedItems will display to the user.
    
    * Added code comments to the SelectedItemTemplate paremeter.
    
    Co-authored-by: AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
    
    * Users/alexsans/ab#86951 make input components read only (#43)
    
    * Added IReadonlyInputComponent interface as a bas class FormInputComponentBase to implement IReadonlyInputComponent interface. Modified the existing inputs to use base class FormInputComponentBase.
    
    * Added the FormInputComponentBase to the FormInputNumber.
    
    * Fixed build errors in the Demo project.
    
    * Removed unnecesary code. Added readonly input examples in the Demo.
    
    Co-authored-by: Yong Guan <67647301+yongguan2001@users.noreply.github.com>
    Co-authored-by: ramtinhosseiniTC <67646952+ramtinhosseiniTC@users.noreply.github.com>
    Co-authored-by: ramtin.hosseini <ramtin.hosseini@034gc.onmicrosoft.com>
    Co-authored-by: TC-Cory <80551174+TC-Cory@users.noreply.github.com>

---
+++ DemoWebsites/BlazorComponentDemoTestSite/DSD.MSS.Blazor.Components.DemoSite/Pages/Input.razor.cs
@@ -0,0 +1,51 @@
+﻿namespace DSD.MSS.Blazor.Components.Core.Demo.Pages
+{
+    using DSD.MSS.Blazor.Components.Core.Demo.Data;
+    using DSD.MSS.Blazor.Components.Core.Demo.Model;
+    using DSD.MSS.Blazor.Components.Core.Models;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.AspNetCore.Components.Forms;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public partial class Input
+    {
+        [Inject]
+        FormInputList InputList { get; set; }
+
+        protected FormInputModel FormInputObj { get; set; }
+
+        protected FormInputModel[] FormInputListObj { get; set; }
+
+        protected EditContext editContext;
+        protected List<SelectListItem> selectlist { get; set; } = new List<SelectListItem>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            FormInputObj = new FormInputModel();
+            FormInputListObj = await InputList.GetFormInputListAsync(DateTime.Now);
+            editContext = new EditContext(FormInputObj);
+            selectlist.Add(new SelectListItem()
+            {
+                Text = "A",
+                Value = true
+            });
+            selectlist.Add(new SelectListItem()
+            {
+                Text = "B",
+                Value = false
+            });
+        }
+
+        private void HandleValidSubmit()
+        {
+            var isValid = editContext.Validate();
+            Console.WriteLine("Valid Submit");
+        }
+        private void OnInputChange(EventArgs e)
+        {
+
+        }
+    }
+}
