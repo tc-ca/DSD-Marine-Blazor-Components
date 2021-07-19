59d95af3cf34b741089d132cc54ea81742b2744b
TC-Cory <80551174+TC-Cory@users.noreply.github.com>
2021-07-19 12:43:06 -0400

    Users/cheniec/ab#105023/history status component (#46)
    * Initial commit of Status component
    
    * Update RequestHistoryComponent.razor
    
    * fixed name spaces
    
    * Modified CSS, HTML
    
    * fixed CSS styling - changed date type to DateTime
    
    * Add check for if list is empty
    
    * Update component to check if list is empty
    
    * Changed Assigned to ChangedBy to reflect changes. Fixed styling
    
    * Edited CSS, changed code behind
    
    * Changed date type to DateTimeOffset

---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core/Components/RequestHistoryComponent.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace DSD.MSS.Blazor.Components.Core.Components
+{
+    using DSD.MSS.Blazor.Components.Core.Models;
+    using DSD.MSS.Blazor.Components.Core.Constants;
+    using Microsoft.AspNetCore.Components;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    public partial class RequestHistoryComponent
+    {
+        [Parameter]
+        public List<StatusHistoryItem> StatusHistories { get; set; }
+        [Parameter]
+        public string DateFormat { get; set; }
+
+        public bool IsListEmpty() => this.StatusHistories == null ? false : true;
+    }
+}
---
+++ BlazorCore/DSD.MSS.Blazor.Components.Core/Models/StatusHistoryItem.cs
@@ -0,0 +1,26 @@
+﻿
+using DSD.MSS.Blazor.Components.Core.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSD.MSS.Blazor.Components.Core.Models
+{
+    /// <summary>
+    /// Model used for populating information in RequestHistory Component
+    /// </summary>
+    public class StatusHistoryItem
+    {
+        public string Id { get; set; }
+
+       public string StatusText { get; set; }
+
+        public string ProcessingPhase { get; set; }
+
+        public RequestStatus RequestStatus { get; set; }
+
+        public DateTimeOffset? RequestStatusTime { get; set; }
+        public string ChangedBy { get; set; }
+    }
+}
