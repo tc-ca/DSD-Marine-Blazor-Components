13484fc9421fc7865fd73a5c0950bbf290231dee
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2021-08-30 13:12:03 -0400

    Users/alexsans/ab#122632 loading button (#59)
    * Added loading button to the component library.
    
    * Added the loading button component to the list of useable components. Added a new page to showcase the custom buttons.

---
+++ DemoWebsites/BlazorComponentDemoTestSite/DSD.MSS.Blazor.Components.DemoSite/Pages/Buttons.razor.cs
@@ -0,0 +1,22 @@
+﻿namespace DSD.MSS.Blazor.Components.Core.Demo.Pages
+{
+    using System.Threading.Tasks;
+
+    public partial class Buttons
+    {
+        public bool Button1Loading { get; set; }
+
+        public bool Button2Loading { get; set; }
+
+        protected async Task Button1OnClick(bool isLoading)
+        {
+            await Task.Delay(5000);
+            this.Button1Loading = false;
+        }
+        protected async Task Button2OnClick(bool isLoading)
+        {
+            await Task.Delay(3000);
+            this.Button2Loading = false;
+        }
+    }
+}
