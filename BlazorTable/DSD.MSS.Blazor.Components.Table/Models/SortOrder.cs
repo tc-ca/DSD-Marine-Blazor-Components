829fac102aa5cc5b134efe92ac3fc4370295c6ed
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2021-11-04 11:15:08 -0400

    Added ability to set a different value for the filter sort order than the display value. Added the ability to sort filter's ascendingly and descendingly. (#80)

---
+++ BlazorTable/DSD.MSS.Blazor.Components.Table/Models/SortOrder.cs
@@ -0,0 +1,23 @@
+﻿namespace DSD.MSS.Blazor.Components.Table.Models
+{
+    /// <summary>
+    /// Defines a sort order.
+    /// </summary>
+    public enum SortOrder
+    { 
+        /// <summary>
+        /// Sort in ascending order.
+        /// </summary>
+        ASCENDING,
+
+        /// <summary>
+        /// Sort in descending order.
+        /// </summary>
+        DESCENDING,
+
+        /// <summary>
+        /// No sort applied.
+        /// </summary>
+        NONE,
+    }
+}
